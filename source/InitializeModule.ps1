51c1dd701c362430592e6ba0e276accde1f3cab4
indented-automation <chris@indented.co.uk>
2017-01-09 12:19:57 +0000

    Module base files

---
+++ source/InitializeModule.ps1
@@ -0,0 +1,7 @@
+function InitializeModule {
+    # Resolver (Message): Initialize the DNS cache for Get-Dns
+    Initialize-InternalDnsCache
+
+    # Resolver (Message): Set a variable to store TC state.
+    New-Variable DnsTCEndFound -Scope Script -Value $false
+}
