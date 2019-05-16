a1c17d24029f62ad8659399b845c683a89a7fcfd
Chris Dent <chris@indented.co.uk>
2019-05-16 14:22:08 +0100

    Adds MR record parser and tests

---
+++ Indented.Net.Dns/class/Resolver/8.DnsMRRecord.ps1
@@ -0,0 +1,32 @@
+class DnsMRRecord : DnsResourceRecord {
+    <#
+                                        1  1  1  1  1  1
+          0  1  2  3  4  5  6  7  8  9  0  1  2  3  4  5
+        +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+        /                    NEWNAME                    /
+        /                                               /
+        +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+
+        http://www.ietf.org/rfc/rfc1035.txt
+    #>
+
+    [RecordType] $RecordType = [RecordType]::MR
+    [String]     $MailboxName
+
+    DnsMRRecord() : base() { }
+    DnsMRRecord(
+        [DnsResourceRecord]  $dnsResourceRecord,
+        [EndianBinaryReader] $binaryReader
+    ) : base(
+        $dnsResourceRecord,
+        $binaryReader
+    ) { }
+
+    Hidden [Void] ReadRecordData([EndianBinaryReader] $binaryReader) {
+        $this.MailboxName = $binaryReader.ReadDnsDomainName()
+    }
+
+    [String] RecordDataToString() {
+        return $this.MailboxName
+    }
+}
