19c49dcf14a8da3f35f699453a1b4557a586a317
Chris Dent <chris@indented.co.uk>
2019-06-18 16:42:04 +0100

    Adds KX and NSAP

---
+++ Indented.Net.Dns/class/Resolver/2.RecordTypes/DnsKXRecord.ps1
@@ -0,0 +1,39 @@
+class DnsKXRecord : DnsResourceRecord {
+    <#
+                                        1  1  1  1  1  1
+          0  1  2  3  4  5  6  7  8  9  0  1  2  3  4  5
+        +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+        |                  PREFERENCE                   |
+        +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+        /                   EXCHANGER                   /
+        /                                               /
+        +--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+--+
+
+        http://www.ietf.org/rfc/rfc2230.txt
+    #>
+
+    [RecordType] $RecordType = [RecordType]::KX
+    [UInt16]     $Preference
+    [String]     $Exchanger
+
+    DnsKXRecord() : base() { }
+    DnsKXRecord(
+        [DnsResourceRecord]  $dnsResourceRecord,
+        [EndianBinaryReader] $binaryReader
+    ) : base(
+        $dnsResourceRecord,
+        $binaryReader
+    ) { }
+
+    hidden [Void] ReadRecordData([EndianBinaryReader] $binaryReader) {
+        $this.Preference = $binaryReader.ReadUInt16($true)
+        $this.Exchanger = $binaryReader.ReadDnsDomainName()
+    }
+
+    hidden [String] RecordDataToString() {
+        return '{0} {1}' -f @(
+            $this.Preference.ToString().PadRight(5, ' '),
+            $this.Exchanger
+        )
+    }
+}
